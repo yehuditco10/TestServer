69ce0ba3350e98d1c15cbd44ca6c0f7c032841d5
yehudit <cyehudit10@gmail.com>
2019-09-03 09:10:04 +0300

    the first push

---
+++ server/BLL/CRUD/CategoryCRUD.cs
@@ -0,0 +1,17 @@
+﻿using DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.CRUD
+{
+    public static class CategoryCRUD
+    {
+        public static List<Category> GetAllCategories(checkTestEntities ctx)
+        {
+            return ctx.Categories.ToList();
+        }
+    }
+}
