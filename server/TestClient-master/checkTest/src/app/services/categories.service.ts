b64b5dcb43e890a963d9951abd2608cf91a34272
קרסניצקי <@DESKTOP-B7R23R0>
2019-09-05 19:16:51 +0300

    angular project

---
+++ server/TestClient-master/checkTest/src/app/services/categories.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CategoriesService {
+
+  constructor(private http: HttpClient) { }
+
+  getCategories() {
+    return this.http.get(environment.baseRoute + 'Categories');
+  }
+  addCategory(id:number,categoryName:string){
+    alert("inservice");
+    alert(id);
+    alert(categoryName);
+
+    debugger;
+    //שליחת שתי פרמטרים
+    return this.http.get(environment.baseRoute+ "addCategory?id="+id+'&categoryName='+categoryName);
+  }
+}
