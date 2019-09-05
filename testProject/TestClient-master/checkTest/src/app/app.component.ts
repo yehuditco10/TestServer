95c5f25ee4dd3dbdb423341ab02803d8eafbe259
programmingprojects11 <programmingprojects11@gmail.com>
2019-09-05 20:33:01 +0300

    highchart fix

--- testProject/TestClient-master/checkTest/src/app/app.component.ts
+++ testProject/TestClient-master/checkTest/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import * as Highcharts from 'highcharts';
+
 
 @Component({
   selector: 'app-root',
@@ -8,55 +8,55 @@ import * as Highcharts from 'highcharts';
 })
 export class AppComponent implements OnInit {
   ngOnInit(): void {
-  //   var x=[{
-  //     name: 'Brands',
-  //     colorByPoint: true,
-  //     data: [{
-  //         name: 'Chrome',
-  //         y: 61.41,
-  //         sliced: true,
-  //         selected: true
-  //     }, {
-  //         name: 'Internet Explorer',
-  //         y: 11.84
-  //     }, {
-  //         name: 'Firefox',
-  //         y: 10.85
-  //     }, {
-  //         name: 'Edge',
-  //         y: 4.67
-  //     }, {
-  //         name: 'Safari',
-  //         y: 4.18
-  //     }, {
-  //         name: 'Other',
-  //         y: 7.05
-  //     }]}];
-  //   Highcharts.chart('container', {
-  //     chart: {
-  //         plotBackgroundColor: null,
-  //         plotBorderWidth: null,
-  //         plotShadow: false,
-  //         type: 'pie'
-  //     },
-  //     title: {
-  //         text: 'Browser market shares in January, 2018'
-  //     },
-  //     tooltip: {
-  //         pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
-  //     },
-  //     plotOptions: {
-  //         pie: {
-  //             allowPointSelect: true,
-  //             cursor: 'pointer',
-  //             dataLabels: {
-  //                 enabled: false
-  //             },
-  //             showInLegend: true
-  //         }
-  //     },
-  //     series: []
-  // });
+    var Highcharts = require('highcharts');
+    Highcharts.chart('container', {
+      chart: {
+          plotBackgroundColor: null,
+          plotBorderWidth: null,
+          plotShadow: false,
+          type: 'pie'
+      },
+      title: {
+          text: 'Browser market shares in January, 2018'
+      },
+      tooltip: {
+          pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+      },
+      plotOptions: {
+          pie: {
+              allowPointSelect: true,
+              cursor: 'pointer',
+              dataLabels: {
+                  enabled: false
+              },
+              showInLegend: true
+          }
+      },
+      series: [{
+        name: 'Brands',
+        colorByPoint: true,
+        data: [{
+            name: 'Chrome',
+            y: 61.41,
+            sliced: true,
+            selected: true
+        }, {
+            name: 'Internet Explorer',
+            y: 11.84
+        }, {
+            name: 'Firefox',
+            y: 10.85
+        }, {
+            name: 'Edge',
+            y: 4.67
+        }, {
+            name: 'Safari',
+            y: 4.18
+        }, {
+            name: 'Other',
+            y: 7.05
+        }]}]
+  });
  }
    title = 'checkTest';
 
