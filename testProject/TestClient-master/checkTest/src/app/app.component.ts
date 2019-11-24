4759ed814407ab16394f1de33d73a1ba5c916ff7
Batel Ayala Bezalel <batelayala1@gmail.com>
2019-11-24 19:09:15 +0200

    fix test without header

--- testProject/TestClient-master/checkTest/src/app/app.component.ts
+++ testProject/TestClient-master/checkTest/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { GlobalVariables } from './global/global-variable';
+import { Router } from '@angular/router';
 
 
 @Component({
@@ -8,16 +9,21 @@ import { GlobalVariables } from './global/global-variable';
   styleUrls: ['./app.component.less']
 })
 export class AppComponent implements OnInit {
-    
-    constructor(private globalVariable: GlobalVariables){    
+
+
+  constructor(private globalVariable: GlobalVariables, private router: Router) {
+    router.events.subscribe((url:any) => {
+      if (url.url.includes("/test/"))
+        this.displayHeader = false;
+      else
+        this.displayHeader = true;
     }
-
+    );
+  }
+  displayHeader: boolean = true;
   ngOnInit(): void {
-  var token = localStorage.getItem("token");
-  if (token != null && token != "")
-    this.globalVariable.setToken(token);
- }
-//    title = 'checkTest';
-
-
+    var token = localStorage.getItem("token");
+    if (token != null && token != "")
+      this.globalVariable.setToken(token);
+  }
 }
